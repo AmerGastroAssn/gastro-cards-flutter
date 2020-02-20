1faa6091d1175385ab5974f108451f23d57eee3b
oliver-gomes <ogomesphoto@gmail.com>
2020-02-20 12:39:26 -0500

    mainpage name change

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
